e43892c046862ed0cc2edea9e3b5850f37d9b544
Samuel <58521841+IronHead43@users.noreply.github.com>
2021-01-21 20:35:54 +0000

    Add files via upload

---
+++ yt -parse-ID.py
@@ -0,0 +1,16 @@
+inpt = input("")
+parse = ""
+#input format: https://www.youtube.com/test (20) or https://youtu.be/ (12)
+if inpt[12] == "y":
+    i = 24
+    while i<len(inpt):
+        parse = parse + inpt[i]
+        i=i+1
+if inpt[8] == "y":
+    i = 17
+    while i<len(inpt):
+        parse = parse + inpt[i]
+        i=i+1
+print(parse)
+
+#GET YOUTUBE ID, sololearn.com challenge

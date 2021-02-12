9c1dc3ee2ff24f3ad290b4edc41cf7dba4f2b746
Samuel <58521841+IronHead43@users.noreply.github.com>
2021-02-12 18:12:42 +0000

    Update cookie clicker.py
    now displays clicks in gui as well as in shell

--- cookie clicker.py
+++ cookie clicker.py
@@ -9,17 +9,20 @@ def buttonDouble():
     if db == 2:
         print("already baught")
     if db == 1:
-        if clicks > 250:
+        if click > 250:
             db = 2
             print("double clicks baught")
-        if clicks < 250:
+        if click < 250:
             print("more coins required, you need 250")
 def buttonClick():
     global click
     click += 1 * db
     print("Cookie Clicks: " + str(click))
+    label.configure(text="clicks: " + str(click))
 mainClickButton = Button(master, text="Click!", command = buttonClick)
 mainBuyButton = Button(master, text="Buy Double Clicks", command = buttonDouble)
+label = Label(text="clicks: " + str(click))
+label.pack()
 mainClickButton.pack()
 mainBuyButton.pack()
 mainloop()

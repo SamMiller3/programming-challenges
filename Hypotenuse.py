f12d55a701164c2c1d02d27213b9a0a400a00058
Samuel <58521841+IronHead43@users.noreply.github.com>
2021-04-22 21:56:46 +0100

    Add files via upload

---
+++ Hypotenuse.py
@@ -0,0 +1,9 @@
+import math
+#Pythagorean Theorem
+#solve for hypotenuse, 22/04/2021
+#c = sqrt(a^2+b^2)
+print("Pythagoras Theorm psuedo code: sqrt(a^2+b^2)")
+a = int(input("value for a: "))
+b = int(input("value for b: "))
+c = math.sqrt((a**2)+(b**2))
+print(c)

5df2316fe97623fb6a96289d756ce82459a93a86
Samuel <58521841+IronHead43@users.noreply.github.com>
2021-04-24 17:05:12 +0100

    better documentation
    added comments to explain better

--- quadratic & linear equations.py
+++ quadratic & linear equations.py
@@ -1,14 +1,18 @@
-offsety = "null"
-div = "null"
-while offsety != "bottom" and offsety != "top":
-    offsety = input("start from bottom or top of screen: ")
-while div != "true" and div != "false":
-    print("divide output by 100? displays more on screen but trades off accuracy (reccomended for big equations but not for smaller/linear ones)")
-    div = input("input either true of false: ")
-if div == "true":
-    div = 100
-if div == "false":
-    div = 1
+#quadraction & linear equations, first made December 2021.
+#Declare variables
+offset_y = "null"
+divide_by_100 = "null"
+#set offset_y, if your going doown it's good to start from the top otherwise start from the bottom
+while offset_y != "bottom" and offset_y != "top":
+    offset_y = input("start from bottom or top of screen: ")
+#divide value by 100? (useful for big equations)
+while divide_by_100 != "true" and divide_by_100 != "false":
+    print("divide_by_100ide output by 100? displays more on screen but trades off accuracy (reccomended for big equations but not for smaller/linear ones)")
+    divide_by_100 = input("input either true of false: ")
+if divide_by_100 == "true":
+    divide_by_100 = 100
+if divide_by_100 == "false":
+    divide_by_100 = 1
 #formula example: y=10^3x + -2^3 + 2    example formula y=(ax)^b + (cx)^d + e
 print("example formula y=ax^b + cx^d + e")
 a = int(input("value of a: "))
@@ -16,25 +20,27 @@ b = int(input("value of b: "))
 c = int(input("value of c: "))
 d = int(input("value of d: "))
 e = int(input("value of e (y intercept): "))
+#how many times it will repeat
 repeat = int(input("how many increments? "))
+#import drawing function
 from turtle import *
 pu()
 setx(0)
-if offsety == "bottom":
-    offsety = -450
-if offsety == "top":
-    offsety = 450
-sety(offsety)
+#goes to starting position
+if offset_y == "bottom":
+    offset_y = -450
+if offset_y == "top":
+    offset_y = 450
+sety(offset_y)
 pd()
 i=0
 print("start")
 print(i<repeat)
 setx(1)
+#executes equation
 while i<repeat:
-    print((a*xcor()**b + c*xcor()**d + e)/div)
-    sety((a*xcor()**b + c*xcor()**d + e + offsety)/div)
+    print((a*xcor()**b + c*xcor()**d + e)/divide_by_100)
+    sety((a*xcor()**b + c*xcor()**d + e + offset_y)/divide_by_100)
     setx(xcor()+1)
     i+=1
 print("end")
-
-

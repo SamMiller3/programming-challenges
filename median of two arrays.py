c44ff3569481986ab43fdf3fa74a8614d18a2b21
Samuel <58521841+IronHead43@users.noreply.github.com>
2022-05-14 13:47:24 +0100

    Delete median of two arrays.py

--- median of two arrays.py
+++
@@ -1,9 +0,0 @@
-#median of two arrays - challange from leetcode.com
-nums1 = list(input())
-nums2 = list(input())
-nums = nums1 + nums2
-nums.sort()
-if len(nums)/2 % 2 == 0:
-    print("median is: " + str(nums[round(len(nums)/2)-1]) + ", " + str(nums[round(len(nums)/2)]))
-else:
-    print("median is: " + str(nums[len(nums)/2]))

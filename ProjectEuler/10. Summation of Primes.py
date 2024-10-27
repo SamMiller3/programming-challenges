f6ed8691461c9cdc5adbf31fb76361d5515cee3b
Samuel <58521841+SamMiller3@users.noreply.github.com>
2024-10-27 10:27:49 +0000

    Update 10. Summation of Primes.py

--- ProjectEuler/10. Summation of Primes.py
+++ ProjectEuler/10. Summation of Primes.py
@@ -1,20 +1,17 @@
 #Project Euler - 10. Summation of Primes
-#23/10/2024
+#26/10/2024
 
 import math
 
-n = 2000000 
-sieve = [True] * n  
-sieve[0] = sieve[1] = False  
-
-# Sieve of Eratosthenes
-for i in range(2, math.isqrt(n) + 1):
-    if sieve[i]:  # If i is still marked as prime
-        for j in range(i * i, n, i):  # Mark multiples of i as non-prime
-            sieve[j] = False
-
-# Collect all primes into a list
-primes = [i for i, is_prime in enumerate(sieve) if is_prime]
-
-
-print(sum(primes))
+n = 2000000
+sieve = [True]*n
+sieve[0]=sieve[1]=False
+for i in range(2,math.isqrt(n)):
+    if sieve[i]==True:
+        for j in range((i*i),n,i):
+            sieve[j]=False
+sum=0
+for i in range(len(sieve)):
+    if sieve[i]:
+        sum+=i
+print(sum)

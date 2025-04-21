cb97e6fd924876d5cb9963584acbf322fb9c1c81
Samuel <58521841+SamMiller3@users.noreply.github.com>
2025-04-21 12:29:56 +0100

    Delete Calcalute Index of Fibonacci Sequence.py

--- Calcalute Index of Fibonacci Sequence.py
+++
@@ -1,6 +0,0 @@
-#Calcalute Index of Fibonacci Sequence, 2/5/2021
-#Uses Binet's Law
-#((1.61803398875^n) - (-1/1.61803398875)^n)/√5
-import math
-n = int(input("input index to calculate: "))
-print(round(((1.61803398875**n) - (-1/1.61803398875)**n)/math.sqrt(5)))

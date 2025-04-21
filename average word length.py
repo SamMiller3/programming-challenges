920552e6491b1aed132859db5b84c5bc10297b28
Samuel <58521841+SamMiller3@users.noreply.github.com>
2025-04-21 17:01:08 +0100

    Delete average word length.py

--- average word length.py
+++
@@ -1,9 +0,0 @@
-#average word length, 21/03/2021, problem from towardsdatascience.com / sololearn.com
-sentance = input()
-sentance = sentance.split()
-i = 0
-average = 0
-for i in range(len(sentance)):
-    average += len(sentance[i])
-average = average/len(sentance)
-print("average word length: " + str(average))

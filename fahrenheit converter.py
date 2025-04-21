1a6b699adcba12a7c30033dbc27a4720272e5834
Samuel <58521841+SamMiller3@users.noreply.github.com>
2025-04-21 13:25:50 +0100

    Delete fahrenheit converter.py

--- fahrenheit converter.py
+++
@@ -1,9 +0,0 @@
-#celsius to fahrenheit (32°C × 9/5) + 32 = 89.6°F   (32°F − 32) × 5/9 = 0°C
-while True:
-    to = ""
-    to = input("inputting celsius (c) or fahreneheit (f)?")
-    inpt = int(input("input a number"))
-    if to == "c":
-        print ((inpt * 9/5) + 32)
-    if to == "f":
-        print ((inpt - 32) * 5/9)

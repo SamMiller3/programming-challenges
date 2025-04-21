e769c9e19eb589b9e7a16dbd290b171ae45147a0
Samuel <58521841+SamMiller3@users.noreply.github.com>
2025-04-21 13:24:36 +0100

    Delete facecar.cs

--- facecar.cs
+++
@@ -1,9 +0,0 @@
-//face car, 5th febuary 2021, C#/unity code
-        if (pause.paused == 0){
-            Vector3 Carpos = Car.transform.position - transform.position;
-         float angle = Mathf.Atan2(Carpos.y, Carpos.x) * Mathf.Rad2Deg - 90;
-         Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
-        transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * 1000);
-        transform.position += transform.up * Time.deltaTime * 0.4f;
-        offsetx = transform.position.x;
-        offsety = transform.position.y;

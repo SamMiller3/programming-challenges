f8b73a1dbf9fc93c3e045d89f33e91849b0edcde
Samuel <58521841+SamMiller3@users.noreply.github.com>
2025-04-21 17:05:20 +0100

    Delete recursionExample.py

--- recursionExample.py
+++
@@ -1,9 +0,0 @@
-#Recursion Example, 10/04/2021
-def count(n):
-    if 0 < n:
-        print(n)
-        count(n-1)
-    else:
-        return(n)
-number = int(input("input a number"))
-count(number)

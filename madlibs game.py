439d19079fae4d2011e8f986687cbee1c529bb45
Samuel <58521841+SamMiller3@users.noreply.github.com>
2025-04-21 12:59:55 +0100

    Delete madlibs game.py

--- madlibs game.py
+++
@@ -1,6 +0,0 @@
-#madlibs game
-#the ad name went over to the noun
-ad = input("enter an adjective")
-noun = input("enter a noun")
-name = input ("enter a name")
-print ("the " + ad + " " + name + " went over to the " + noun)

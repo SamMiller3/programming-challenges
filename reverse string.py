4e02b7dfcf3e6464816aa442f88ad362cbf9c642
Samuel <58521841+SamMiller3@users.noreply.github.com>
2025-04-22 17:39:41 +0100

    Delete reverse string.py

--- reverse string.py
+++
@@ -1,7 +0,0 @@
-#reverse string 19/01/2021, challenge from quora.com
-while 1>0:
-    inpt=input("input a string")
-    out=""
-    for i in range(len(inpt)):
-        out=inpt[i]+out
-    print("reversed string: " + out)

1a341d8ed76b5f440a36ec119726a852ef691b66
Samuel <58521841+SamMiller3@users.noreply.github.com>
2025-12-06 14:56:02 +0000

    Extract logic into part_one and part_two functions
    Refactor code into functions for part one and part two.

--- Advent of Code/Day 1.py
+++ Advent of Code/Day 1.py
@@ -1,15 +1,42 @@
 # Advent of Code 2025 Day 1
-password = 0
-num = 50 
-with open("input", "r") as file:
-    for line in file:
-        turn = line.strip()
-        direction = turn[0]
-        magnitude = int(turn[1:]) 
-        if direction == "L":
-            num = (num - magnitude) % 100
-        else:
-            num = (num + magnitude) % 100
-        if num == 0: password+=1
-print(password)
 
+# part one
+
+def part_one():
+    password = 0
+    num = 50 
+    with open("input", "r") as file:
+        for line in file:
+            turn = line.strip()
+            direction = turn[0]
+            magnitude = int(turn[1:]) 
+            if direction == "L":
+                num = (num - magnitude) % 100
+            else:
+                num = (num + magnitude) % 100
+            if num == 0: password+=1
+    return(password)
+
+# part two
+
+def part_two():
+    password = 0
+    num = 50 
+    with open("input", "r") as file:
+        for line in file:
+            turn = line.strip()
+            direction = turn[0]
+            m = int(turn[1:]) 
+            if direction == 'R':
+                start = num
+                end = num + m
+                password += end // 100 - start // 100
+                num = end % 100
+
+            else: 
+                start = 99 - num
+                end = 99 - (num - m)
+                password += end // 100 - start // 100
+                num = (num - m) % 100
+
+    return(password)
